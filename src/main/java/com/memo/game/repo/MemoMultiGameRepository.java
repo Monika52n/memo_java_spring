6ce213713b86d875b7595af7b8f91743e47b0166
Nick Mónika <nickmonikavera@gmail.com>
2024-04-18 01:32:30 +0200

    mp 6

--- src/main/java/com/memo/game/repo/MemoMultiGameRepository.java
+++ src/main/java/com/memo/game/repo/MemoMultiGameRepository.java
@@ -4,7 +4,10 @@ import com.memo.game.entity.MemoMultiGame;
 import com.memo.game.entity.MemoSingleGame;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface MemoMultiGameRepository extends JpaRepository<MemoMultiGame, UUID> {
+    List<MemoMultiGame> findByPlayer1(UUID player1);
+    List<MemoMultiGame> findByPlayer2(UUID player2);
 }
