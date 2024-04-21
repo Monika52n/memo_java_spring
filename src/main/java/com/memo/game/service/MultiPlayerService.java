d47e985971c6368fc5dd86a799ecf016d63fff15
Nick Mónika <nickmonikavera@gmail.com>
2024-04-22 01:28:20 +0200

    mp 10

--- src/main/java/com/memo/game/service/MultiPlayerService.java
+++ src/main/java/com/memo/game/service/MultiPlayerService.java
@@ -14,7 +14,6 @@ import java.util.UUID;
 @Service
 public class MultiPlayerService {
     private final List<MultiPlayer> games = new ArrayList<MultiPlayer>();
-    private final List<UUID> waitingPlayers = new ArrayList<UUID>();
 
     private final MemoMultiGameRepository memoMultiGameRepository;
 
@@ -37,31 +36,18 @@ public class MultiPlayerService {
                     && game.getNumberOfPairs()==numberOfPairs) {
                 game.setPlayer2Id(player);
                 game.setGameStarted(true);
-                //game.setGameState(GameState.PLAYER1_TURN);
-                printDebug();
                 return game;
             }
         }
 
         MultiPlayer game = new MultiPlayer(numberOfPairs, player, null);
         games.add(game);
-        waitingPlayers.add(player);
-
-        printDebug();
         return game;
     }
 
-    private void printDebug() {
-        System.out.println(waitingPlayers);
-        for(MultiPlayer gameP : games) {
-            System.out.print(gameP.toString() + " ");
-        }
-        System.out.print("\n");
-    }
     public synchronized MultiPlayer leaveGame(UUID player) {
         MultiPlayer game = getGameByPlayer(player);
         if (game != null) {
-            waitingPlayers.remove(player);
             game.playerLeaves(player);
             if(game.isGameOver()) {
                 if(game.isGameStarted()) {
@@ -69,7 +55,6 @@ public class MultiPlayerService {
                 }
                 games.remove(game);
             }
-            printDebug();
             return game;
         }
         return null;
@@ -83,6 +68,7 @@ public class MultiPlayerService {
     }
 
     public MultiPlayer getGame(UUID gameId) {
+        debug();
         if(gameId==null) return null;
         return games.stream().filter(game -> gameId.equals(game.getPlayId())).findFirst().orElse(null);
     }
@@ -90,6 +76,12 @@ public class MultiPlayerService {
     public void removeGame(UUID gameId) {
         MultiPlayer game = getGame(gameId);
         games.remove(game);
+    }
+
+    public void debug() {
+        for(MultiPlayer game : games) {
+            System.out.println(game);
+        }
     }
 
     public void saveGame(MultiPlayer game) {
